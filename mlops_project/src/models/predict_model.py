4b3735b918bad7bb4da62eea80573bee716552b7
Aswin Anil Varkey <aswinanilvarkey@gmail.com>
2022-01-05 16:21:38 +0100

    reproducable model

--- mlops_project/src/models/predict_model.py
+++ mlops_project/src/models/predict_model.py
@@ -9,54 +9,21 @@ from torch import nn, optim
 
 from src.models.model import MyAwesomeModel
 
+import hydra
+from hydra.utils import get_original_cwd
 
-def test():
-    testloader = torch.load("data/processed/test_loader.pth")
+import logging
+log = logging.getLogger(__name__)
+@hydra.main(config_path = '../../src/config/' , config_name="testconfig")
 
-    print("Training day and night")
-    parser = argparse.ArgumentParser(description="Training arguments")
-    parser.add_argument("--lr", default=0.1)
-    # add any additional argument that you want
-    args = parser.parse_args(sys.argv[2:])
-    print(args)
-
-    # Implement training loop here
-    NN = MyAwesomeModel()
-    model = torch.load("models/checkpoint1.pth")
-
-    print("Evaluating until hitting the ceiling")
+def predict(cfg):
+    testpath = cfg.hyperparameters.testpath
+    checkpoint = cfg.hyperparameters.checkpoint
+    
     model = MyAwesomeModel()
-    criterion = nn.NLLLoss()
-    with torch.no_grad():
-        cnt = []
-        running_loss = 0
-        i = 0
-        for images, labels in testloader:
-            log_ps = model(images)
-            loss = criterion(log_ps, labels.long())
-            running_loss += loss.item()
-            _, top_class = torch.exp(log_ps).topk(1, dim=1)
-            equals = top_class == labels.view(*top_class.shape)
-            cnt.append(torch.mean(equals.type(torch.FloatTensor)))
-            accuracy = torch.mean(equals.type(torch.FloatTensor))
-            print(f'Accuracy: {accuracy.item()*100}%')
-            i+=1
-        acc = torch.sum(torch.tensor(cnt)) / len(cnt)
-        print(f"Accuracy: {acc*100:.2f}%")
-
-    torch.save(NN.state_dict(), "models/checkpoint.pth")
-
-
-def predict():
-    parser = argparse.ArgumentParser(description='Training arguments')
-    parser.add_argument('--model_path', default="models/checkpoint.pth")
-    parser.add_argument('--folder_path',default="data/processed/test_1.pth")
-    # add any additional argument that you want
-    args = parser.parse_args(sys.argv[2:])
-    print(args)
-    testloader = torch.load("data/processed/test_loader.pth")
-    model = MyAwesomeModel()
-    model.load_state_dict(torch.load("models/checkpoint1.pth"))
+    model.load_state_dict(torch.load(str(get_original_cwd()) + checkpoint))
+    testloader = torch.load(str(get_original_cwd()) +testpath)
+    model.eval()
     with torch.no_grad():
         cnt = 0
         for images, labels in testloader:  
@@ -64,11 +31,12 @@ def predict():
                 img = img.view(1,28,28) 
                 log_ps = model(img)
                 _, top_class = torch.exp(log_ps).topk(1, dim=1)
-                print(f"Label: {labels[idx]}, Prediction: {top_class.item()}")
+                log.info(f"Label: {labels[idx]}, Prediction: {top_class.item()}")
                 if labels[idx]==top_class:
                     cnt+=1
             break
-        print(f'Accuracy: {cnt/idx*100:.2f}%')
+        log.info(f'Accuracy: {cnt/idx*100:.2f}%')
+    model.train()
 
 if __name__ == "__main__":
     predict()
--- mlops_project/src/models/train_model.py
+++ mlops_project/src/models/train_model.py
@@ -1,7 +1,6 @@
 import argparse
 import sys
 
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 #from model import MyAwesomeModel
@@ -10,20 +9,29 @@ import click
 
 from src.models.model import MyAwesomeModel
 
-@click.command()
-@click.argument('epochs', default=20)
-@click.argument('data', default='train_loader')
+import hydra
+from hydra.utils import get_original_cwd
 
-def train(epochs, data):
-    train_set = torch.load("data/processed/" + data + ".pth")
+import logging
+log = logging.getLogger(__name__)
 
-    print("Training day and night")
+@hydra.main(config_path = '../../src/config/',config_name="trainconfig")
+
+def train(cfg):
+    trainpath = cfg.hyperparameters.trainpath
+    checkpoint = cfg.hyperparameters.checkpoint
+    epochs = cfg.hyperparameters.epochs
+    lr = cfg.hyperparameters.lr
+    
+    train_set = torch.load(str(get_original_cwd()) +trainpath)
+
+    log.info("Training day and night")
     epochs = int(epochs)
 
     # Implement training loop here
     NN = MyAwesomeModel()
     criterion = nn.NLLLoss()
-    optimizer = optim.Adam(NN.parameters(), lr=3e-4)
+    optimizer = optim.Adam(NN.parameters(), lr=lr)
     for e in range(epochs):
         running_loss = 0
         for images, labels in train_set:
@@ -35,12 +43,9 @@ def train(epochs, data):
             running_loss += loss.item()
             optimizer.step()
         else:
-            if e == epochs -1:
-                print(torch.exp(output).topk(1, dim=1))
-                print(labels)
-            print(f"Training loss: {running_loss/len(train_set)}")
+            log.info(f"Training loss: {running_loss/len(train_set)}")
 
-    torch.save(NN.state_dict(), "models/checkpoint1.pth")
+    torch.save(NN.state_dict(),str(get_original_cwd()) + checkpoint)
 
 
 if __name__ == "__main__":
