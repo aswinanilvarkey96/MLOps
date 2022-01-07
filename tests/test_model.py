3754734d6da6ae7d118814ce7f1031cc8a757771
Aswin Anil Varkey <aswinanilvarkey@gmail.com>
2022-01-07 11:33:51 +0100

    CI fix

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,9 +3,19 @@ import torch
 from torch import nn, optim
 import numpy as np
 import os
-cwd = os.getcwd()
-print('#############',cwd)
 from src.models.model import MyAwesomeModel
+cwd = os.getcwd()
+
+print('#############',cwd)
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in files:
+    print(f)
+
+
+print('#############')
+files = [f for f in os.listdir('../') if os.path.isfile(f)]
+for f in files:
+    print(f)
 
 train_set = torch.load("data/processed/train_loader.pth")
 data_train, label_train =  next(iter(train_set))    
