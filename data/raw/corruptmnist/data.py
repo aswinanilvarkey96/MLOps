93e6a03b3caab6bb1bf3c47038d7dfcd6fd15baf
Aswin Anil Varkey <aswinanilvarkey@gmail.com>
2022-01-07 11:58:50 +0100

    added data folder

---
+++ data/raw/corruptmnist/data.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  4 11:26:25 2022
+
+@author: aswin
+"""
--- tests/test_model.py
+++ tests/test_model.py
@@ -4,20 +4,9 @@ from torch import nn, optim
 import numpy as np
 import os
 cwd = os.getcwd()
-
-print('#############',cwd)
-files = [f for f in os.listdir('.') ]
-for f in files:
-    print(f)
-
-
-print('#############')
-files = [f for f in os.listdir('../') if os.path.isfile(f)]
-for f in files:
-    print(f)
-
-from src.models.model import MyAwesomeModel
-train_set = torch.load("data/processed/train_loader.pth")
+os.chdir(str(cwd) + "/src/models/")
+from model import MyAwesomeModel
+train_set = torch.load(str(cwd) + "/data/processed/train_loader.pth")
 data_train, label_train =  next(iter(train_set))    
 NN = MyAwesomeModel()
 output = NN(data_train)
