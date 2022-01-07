8ba59dba50cdcc17b338ad028f695a5cc180ea5d
Aswin Anil Varkey <aswinanilvarkey@gmail.com>
2022-01-07 12:07:35 +0100

    CI fix

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,7 +5,10 @@ import numpy as np
 import os
 cwd = os.getcwd()
 os.chdir(str(cwd) + "/src/models/")
-from model import MyAwesomeModel
+ch = str(cwd) + "/src/models/"
+print(os.getcwd())
+from home.runner.worl.MLOps.MLOps.src.models.model import MyAwesomeModel
+os.chdir(str(cwd))
 train_set = torch.load(str(cwd) + "/data/processed/train_loader.pth")
 data_train, label_train =  next(iter(train_set))    
 NN = MyAwesomeModel()
