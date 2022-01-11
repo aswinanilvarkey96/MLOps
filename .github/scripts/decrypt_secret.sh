25324cab5112f17816e47598c32acdebdf82dd8d
Aswin Anil Varkey <aswinanilvarkey@gmail.com>
2022-01-11 15:24:43 +0100

    encryption

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,6 +2,6 @@
 
 # Decrypt the file
 # --batch to prevent interactive command
-# --yes to assume "yes" for questions
+# --yes to assume "yes" for questionsss
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET" \
 --output application_default_credentials.json application_default_credentials.json.gpg
