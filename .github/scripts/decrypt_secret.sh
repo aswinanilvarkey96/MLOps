dc4e8b72bb7bbf6198b2f592070a84c90463733b
Aswin Anil Varkey <aswinanilvarkey@gmail.com>
2022-01-11 16:48:55 +0100

    encryption

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Decrypt the file
 # --batch to prevent interactive command
-# --yes to assume "yes" for questionsss
+# --yes to assume "yes" for questionsssss
 pwd
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET" --output secret.json secret.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET" --output "$pwd.github/scripts/secret.json" "$pwd/.github/scripts/secret.json.gpg"
